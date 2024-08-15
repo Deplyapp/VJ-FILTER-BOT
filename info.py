16978357d8b47b5295abd8d0f18043f94172a039
Deplyapp <151662584+Deplyapp@users.noreply.github.com>
2024-08-15 15:00:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -71,7 +71,7 @@ CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "mongodb+srv://nestplusde
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', 'logs345') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/botnest_movies')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/movie_request_pr')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/botnestsupport')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_To_Open_Linkl')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'kaifsarkar53') # Support Chat Link Without https:// or @
